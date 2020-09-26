e90c218491c68925799b42d420d18c925e5674e0
ouzhanjun <53970429@qq.com>
2020-09-26 09:43:34 +0800

    htmledit.html

--- editor/htmledit.js
+++ editor/htmledit.js
@@ -1,12 +1,37 @@
 var xeditor = function () {
-    var cmdNames = ["bold", "copy", "cut", "selectAll","open","saveAs","print","delete",
-    "insertImage", "formatblock", "backColor"];
-    var keyValues = {};
-    for (var i in cmdNames) {
-        var name = cmdNames[i];
-        this[name] = function (val) {
-            document.execCommand(arguments.callee.cmd, false, val);
+    var editCommands = [
+        "copy", "cut", "paste", "delete",
+        "bold", "strikeThrough", "italic", "underline", "selectAll",
+        "subscript", "superscript",
+        "justifyCenter", "justifyFull", "justifyLeft", "justifyRight",
+        "fontName", "fontSize", "foreColor", "backColor",
+        "indent", "outdent",
+        "removeFormat", "hiliteColor"
+    ];
+    var createCommands = [
+        "createLink", "unlink",
+        "formatblock", "forwardDelete",
+        "insertImage", "insertParagraph", "insertHorizontalRule",
+        "insertText", "insertHTML",
+        "insertOrderedList", "insertUnorderedList",
+        "increaseFontSize", "heading", "decreaseFontSize"
+    ];
+    var controlCommands = [
+        "redo", "undo",
+        "insertBrOnReturn", "ClearAuthenticationCache", "contentReadOnly",
+        "useCSS", "styleWithCSS",
+        "defaultParagraphSeparator",
+        "enableObjectResizing", "enableInlineTableEditing", "enableAbsolutePositionEditor",
+    ];
+    
+    var commandTypes = [editCommands, createCommands, controlCommands];
+    for (var commands in commandTypes) {
+        for (var i in commands) {
+            var name = commands[i];
+            this[name] = function (val) {
+                document.execCommand(arguments.callee.cmd, false, val);
+            }
+            this[name].cmd = name;
         }
-        this[name].cmd = name;
     }
 }
