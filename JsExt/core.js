1f1e2a449415e50cb01d62a638a7ab2dc90daa1a
ouzhanjun <53970429@qq.com>
2020-09-26 23:17:44 +0800

    core.js

--- JsExt/core.js
+++ JsExt/core.js
@@ -4,12 +4,56 @@ var jsDom = function (selector, context) {
 	return new jsDom.fn.init(selector, context);
 };
 
+jsDom.validate = {
+	isArrayLike: function (obj) {
+
+		var length = !!obj && obj.length;
+
+		return Array.isArray(obj) || typeof length === "number" && length > 0 && (length - 1) in obj;
+	},
+	isPlainObject: function (obj) {
+		var proto, Ctor;
+
+		if (!obj || toString.call(obj) !== "[object Object]") {
+			return false;
+		}
+
+		proto = Object.getPrototypeOf(obj);
+
+		if (!proto) {
+			return true;
+		}
+
+		Ctor = proto.hasOwnProperty("constructor") && proto.constructor;
+		return typeof Ctor === "function" && Ctor.toString() === Object.toString();
+	},
+	isEmptyObject: function (obj) {
+		var name;
+
+		for (name in obj) {
+			return false;
+		}
+		return true;
+	},
+	isFunction: function (fn) {
+		return typeof fn === "function" && typeof fn.nodeType !== "number";
+	}
+}
+
+var types = ["Boolean", "Number", "String", "Array", "Date", "RegExp", "Object", "Error", "Symbol"];
+for (var i in types) {
+	var typeName = types[i];
+	if (!jsDom.validate["is" + typeName]) {
+		jsDom.validate["is" + typeName] = function (obj) {
+			return toString.call(obj) === "[object " + typeName + "]";
+		}
+	}
+}
 
 //#region jsDom 对象 定义
 jsDom.prototype = {
 	version: "jsDom 1.0.0",
 	constructor: jsDom,
-
 	AddElems: function (elems) {
 		var ret = jsDom.AddElemsToTarget(this.constructor(), elems);
 		ret["prevObject"] = this;
@@ -93,7 +137,6 @@ jsDom.prototype = {
 	},
 };
 //#endregion
-
 
 jsDom.fn = jsDom.prototype;
 jsDom.extend = jsDom.fn.extend;
