9f3ec2fcdd748c0d15177695f43149ff865038f9
ouzhanjun <53970429@qq.com>
2020-09-26 08:17:52 +0800

    htmledit.js

--- editor/htmledit.js
+++ editor/htmledit.js
@@ -1,18 +1,11 @@
 var xeditor = function () {
-    var cmdNames = ["bold", "copy", "cut", "selectAll","open","saveAs","print","delete"];
-    var cmdNamesWithArgs = ["insertImage", "formatblock", "backColor"];
+    var cmdNames = ["bold", "copy", "cut", "selectAll","open","saveAs","print","delete",
+    "insertImage", "formatblock", "backColor"];
     var keyValues = {};
     for (var i in cmdNames) {
         var name = cmdNames[i];
-        this[name] = function () {
-            document.execCommand(arguments.callee.cmd, false, null);
-        }
-        this[name].cmd = name;
-    }
-    for (var i in cmdNamesWithArgs) {
-        var name = cmdNamesWithArgs[i];
         this[name] = function (val) {
-            document.execCommand(arguments.callee.cmd, false, null);
+            document.execCommand(arguments.callee.cmd, false, val);
         }
         this[name].cmd = name;
     }
