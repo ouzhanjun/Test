2cc7409ced6ab109ce924de9098c953e248fe674
ouzhanjun <53970429@qq.com>
2020-10-02 22:46:28 +0800

    document.js

--- JsExt/document.js
+++ JsExt/document.js
@@ -1,30 +1,22 @@
-jsDom.Document = {
-    onreadystatechange: function () {
+jsDom.Document = function () {
+    
+}
+
+jsDom.Document.ready = function () {
+    document.onreadystatechange = function () {
         switch (document.readyState) {
             case "loading":
                 // document 仍在加载
-                this.readyStateChange.loading();
+                console.log("loading");
                 break;
             case "interactive":
                 // DOMContentLoaded 文档已被解析，"正在加载"状态结束，但是诸如图像，样式表和框架之类的子资源仍在加载
-                console.log("onreadystatechange interactive");
+                console.log("interactive");
                 break;
             case "complete":
                 // load 文档和所有子资源已完成加载。表示 load 状态的事件即将被触发
-                console.log("onreadystatechange complete");
+                console.log("complete");
                 break;
-        }
-    },
-    readyStateChange:{
-        onLoading:function(fn,args){
-            console.log("onLoading");
-        },
-        onInteractive:function(fn,args){
-            console.log("onInteractive");
-        },
-        onComplete:function(fn,args){
-            console.log("onComplete");
         }
     }
 }
-
--- JsExt/event.js
+++ JsExt/event.js
@@ -1,49 +1,36 @@
-
-jsDom.fn.bind = function (target, eventType, fn) {
-	if (typeof eventType !== "string") {
-		return;
+jsDom.Event = new function () {
+	Types = {
+		Mouse: ["click", "contextmenu", "dblclick", "mousedown", "mouseenter", "mouseleave", "mousemove", "mouseover", "mouseout", "mouseup", "onwheel"],
+		Keyboard: ["keydown", "keypress", "keyup"],
+		Page: ["abort", "beforeupload", "error", "hashchange", "load", "pageshow", "pagehide", "resize", "scroll", "unload"],
+		Form: ["blur", "change", "focus", "focusin", "focusout", "input", "reset", "search", "select", "submit"],
+		Print: ["afterprint", "beforeprint"]
 	}
-	eventType = eventType.toLowerCase();
-	if (target.addEventListener) {
-		target.addEventListener(eventType, fn, false);
-	} else if (target.attachEvent) {
-		target.attachEvent('on' + eventType, fn);
-	} else {
-		target['on' + eventType] = fn;
+	bind = function (target, eventType, fn) {
+		if (typeof eventType !== "string") {
+			return;
+		}
+		eventType = eventType.toLowerCase();
+		if (target.addEventListener) {
+			target.addEventListener(eventType, fn, false);
+		} else if (target.attachEvent) {
+			target.attachEvent('on' + eventType, fn);
+		} else {
+			target['on' + eventType] = fn;
+		}
 	}
-}
 
-jsDom.fn.unbind = function (target, eventType, fn) {
-	if (typeof eventType !== "string") {
-		return;
-	}
-	eventType = eventType.toLowerCase();
-	if (target.removeEventListener) {
-		target.removeEventListener(eventType, fn, false);
-	} else if (target.detachEvent) {
-		target.detachEvent('on' + eventType, fn);
-	} else {
-		target['on' + eventType] = null;
-	}
-}
-
-//Document事件
-JDOMDocumentEvent = function (selector) {
-	this.constructor = JDOMDocumentEvent;
-	this.selector = selector;
-
-	this.DOMContentLoaded = function (fn) {
-		if (typeof (fn) === "function") {
-			this.CompleteDomContentLoaded = function (selector) {
-				fn();
-				selector.removeEventListener("DOMContentLoaded", this.CompleteDomContentLoaded);
-			}
-			if (this.selector.readyState !== "loading") {
-				window.setTimeout(fn);
-			}
-			else {
-				this.selector.addEventListener("DOMContentLoaded", this.CompleteDomContentLoaded(this.selector));
-			}
+	unbind = function (target, eventType, fn) {
+		if (typeof eventType !== "string") {
+			return;
+		}
+		eventType = eventType.toLowerCase();
+		if (target.removeEventListener) {
+			target.removeEventListener(eventType, fn, false);
+		} else if (target.detachEvent) {
+			target.detachEvent('on' + eventType, fn);
+		} else {
+			target['on' + eventType] = null;
 		}
 	}
 }
