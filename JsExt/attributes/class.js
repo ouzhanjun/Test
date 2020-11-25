c455c192f6e999015b58f6081e06c381595531d2
jimo <jimo@nextvote.com>
2020-11-25 17:32:02 +0800

    .

--- JsExt/attributes/class.js
+++ JsExt/attributes/class.js
@@ -1,26 +1,41 @@
 
 jsDom.Class = {
 
-    getClass: function (elem) {
-        return elem.getAttribute && elem.getAttribute("class") || "";
+    getClassName: function (elem) {
+        return (elem.getAttribute && elem.getAttribute("class")) || elem.className || "";
     },
-    getClasses: function (elem) {
-        var className = this.getClass(elem);
-        return className ? jsDom.String.splitToArray(className) : [];
+    getClassList: function (elem) {
+        var result = [];
+        if (elem && (result = elem.classList)) {
+            result = Array.prototype.slice.call(result, 0, result.length)
+        }
+        else {
+            var classNames = this.getClassName(elem);
+            result = classNames ? jsDom.String.splitToArray(classNames) : [];
+        }
+        return result;
     },
-    hasClass: function (elem, value) {
+    hasClass: function (elem, className) {
         var i = 0;
-        var classes = getClasses(elem).filter(
-            function (x) {
-                return x === value;
-            });
-        return classes.length > 0;
+        var classes = this.getClassList(elem);
+        return className && classes.indexOf(className) != -1;
     },
-    addClass: function (value) {
-        var classes, elem, cur, curValue, curValues, className, j, finalValue,
+    addClass: function (elems, classNames) {
+        var classes,
+            elem,
+            cur,
+            curValue,
+            curValues,
+            j,
+            finalValue=jsDom.String.splitToArray(classNames).join(" "),
             i = 0;
-
+        
         if (typeof value === "function") {
+            for (i in elems) {
+                elem = elems[i]
+                if()
+                typeof elem  !== "undefined" ? (elem.className = finalValue) : (elem.setAttribute("class", finalValue));
+            }
             return this.each(function (j) {
                 jsDom(this).addClass(value.call(this, j, getClass(this)));
             });
@@ -90,5 +105,5 @@ jsDom.Class = {
 
         return this;
 
-    },
+    }
 }
