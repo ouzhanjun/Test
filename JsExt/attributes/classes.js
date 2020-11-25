d1d7ec2d81200e32a083f8e01b223848a1868802
ouzhanjun <53970429@qq.com>
2020-11-26 00:44:10 +0800

    。

--- JsExt/attributes/classes.js
+++ JsExt/attributes/classes.js
@@ -20,53 +20,26 @@ jsDom.Class = {
         var classes = this.getClassList(elem);
         return className && classes.indexOf(className) != -1;
     },
-    addClass: function (elems, classNames) {
-        var classes,
-            elem,
-            cur,
-            curValue,
-            curValues,
-            j,
-            finalValue=jsDom.String.splitToArray(classNames).join(" "),
-            i = 0;
-        
-        if (typeof value === "function") {
-            for (i in elems) {
-                elem = elems[i]
-                if()
-                typeof elem  !== "undefined" ? (elem.className = finalValue) : (elem.setAttribute("class", finalValue));
-            }
-            return this.each(function (j) {
-                jsDom(this).addClass(value.call(this, j, getClass(this)));
-            });
-        }
+    addClass: function (elems, value) {
+        var elem,
+            curClass,
+            finalValue,
+            classNames = Array.isArray(value) ? value.join(" ") : value;
 
-        classes = jsDom.split(value);
-
-        if (classes.length) {
-            while ((elem = this[i++])) {
-                curValues = this.getClasses(elem);
-                curValue = curValues.join(' ');
-                cur = elem.nodeType === 1;
-
-                if (cur) {
-                    j = 0;
-                    while ((className = classes[j++])) {
-                        if (curValues.indexOf(className) < 0) {
-                            curValues.push(className);
-                        }
-                    }
-
-                    // Only assign if different to avoid unneeded rendering.
-                    finalValue = curValues.join(' ');
-                    if (curValue !== finalValue) {
-                        elem.setAttribute("class", finalValue);
+        if (typeof classNames === "string") {
+            for (var i in elems) {
+                if ((elem = elems[i]) && elem.nodeType === 1) {
+                    curClass = this.getClassName(elem);
+                    classNames += " " + curClass;
+                    finalValue = jsDom.String.splitToArray(classNames).join(" ")
+                    if (finalValue !== curClass) {
+                        typeof elem.className !== "undefined" ? (elem.className = finalValue) : (elem.setAttribute("class", finalValue));
                     }
                 }
             }
         }
 
-        return this;
+        return elems;
     },
     removeClass: function (value) {
         var classes, elem, cur, curValue, curValues, className, j, finalValue, index
